b505ca5f1b476c411f30fceb3f4f8491a7612647
CaptainIndia <40968890+Sakarmit@users.noreply.github.com>
2023-11-05 15:52:03 -0500

    Audio Index Fix

--- Scripts/Communication.gd
+++ Scripts/Communication.gd
@@ -127,15 +127,15 @@ var dialogue = [{"Name":"Mentor", "Text":"Alright, kid. With graduation coming u
 				,{"Name":"Mentor", "Text":"Congratulation kid you are now officially a professional"}
 			]
 			
-var dialogue_audio = ["playerVoiceTest",
+var dialogue_audio = [
 				"0",
 				"1",
 				"2",
 				"3",
 				"4",
 				"5",
-				
 				"6",
+				
 				"7",
 				"8",
 				"9",
@@ -157,14 +157,14 @@ var dialogue_audio = ["playerVoiceTest",
 				"25",
 				"26",
 				"27",
-				
 				"28",
+				
 				"29",
 				"30",
 				"31",
 				"32",
-				
 				"33",
+				
 				"34",
 				"35",
 				"36",
@@ -181,6 +181,7 @@ var dialogue_audio = ["playerVoiceTest",
 				"47",
 				"48",
 				"49",
+				"playerVoiceTest",
 				
 				"playerVoiceTest",
 				"playerVoiceTest",
