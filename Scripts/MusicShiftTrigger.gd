4c91edef15344c3305095659270ec294b132b43e
JusPSLiu <juspsliu@yahoo.com>
2023-11-04 20:51:01 -0400

    music

---
+++ Scripts/MusicShiftTrigger.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@export var music : String = "violins"
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if (body.is_in_group("Radio")):
+		self.queue_free()
+		body._change_music(music)
