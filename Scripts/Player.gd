315904223c49c4a2b7c34dd90bf3429029457767
Sakarmit <40968890+Sakarmit@users.noreply.github.com>
2023-11-04 21:02:10 -0400

    Another attempt at flipping

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,5 +9,6 @@ extends VehicleBody3D
 func _physics_process(delta: float) -> void:
 	steering = lerp(steering, Input.get_axis("right", "left") * MAX_STEER * 0.4, 5 * delta)
 	engine_force = Input.get_axis("back", "forward") * ENGINE_POWER
-	quaternion.z = lerp(quaternion.z, 0.0, delta * 10)
+	quaternion.z = lerp(quaternion.z, 0.0, delta * 5)
+	quaternion.x = lerp(quaternion.x, 0.0, delta * 5)
 
