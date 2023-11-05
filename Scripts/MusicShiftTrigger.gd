b7bce9cf183039ec579838d020d18c09c0ba7ccd
JusPSLiu <juspsliu@yahoo.com>
2023-11-04 23:40:29 -0400

    music and tunnels

--- Scripts/MusicShiftTrigger.gd
+++ Scripts/MusicShiftTrigger.gd
@@ -3,16 +3,6 @@ extends Node3D
 @export var music : String = "violins"
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print_debug("WHAT")
-	if (body.is_in_group("Radio")):
-		print_debug("NOT WORKING")
+	if (body is Player):
 		self.queue_free()
 		body._change_music(music)
-
-
-func _on_area_3d_area_entered(area):
-	print_debug("WHAT")
-	if (area.is_in_group("Radio")):
-		print_debug("NOT WORKING")
-		self.queue_free()
-		area._change_music(music)
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,6 +5,12 @@ extends VehicleBody3D
 @export var MAX_STEER: float = 0.4
 @export var ENGINE_POWER: int = 200
 
+#audio stuff
+@export var musicPlayer : AnimationPlayer
+@export var radio : AudioStreamPlayer3D
+@export var altRadio : AudioStreamPlayer3D
+var currentlyAlternating : bool = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frwwwwwwwwame.
 func _physics_process(delta: float) -> void:
 	steering = lerp(steering, Input.get_axis("right", "left") * MAX_STEER * 0.4, 5 * delta)
@@ -12,3 +18,16 @@ func _physics_process(delta: float) -> void:
 	quaternion.z = lerp(quaternion.z, 0.0, delta * 5)
 	quaternion.x = lerp(quaternion.x, 0.0, delta * 5)
 
+
+func _change_music(music:String):
+	if (currentlyAlternating):
+		radio.volume_db = -80
+		radio.stream = load("res://Audio/Music/"+music+".mp3")
+		radio.play(altRadio.get_playback_position())
+		musicPlayer.play("ToRadio")
+	else:
+		altRadio.volume_db = -80
+		altRadio.stream = load("res://Audio/Music/"+music+".mp3")
+		altRadio.play(radio.get_playback_position())
+		musicPlayer.play("ToAlt")
+	currentlyAlternating = !currentlyAlternating
